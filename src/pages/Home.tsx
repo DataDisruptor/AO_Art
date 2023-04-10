5bc69c7c211d9ed7294402cd2c0a7d5eabd27785
Adam Ocheri <adamotchery@gmail.com>
2023-04-10 09:25:01 +0300

    291

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,7 +47,7 @@ export default function Home() {
   return (
     
     <div className="canvas-container">
-      {/* <Canvas3D targetSubScene={skillView} renderStartCallback={(e) => {assetsLoaded(e)}}/> */}
+      <Canvas3D targetSubScene={skillView} renderStartCallback={(e) => {assetsLoaded(e)}}/>
       {/* {!hasLanded &&
       <div className="j-center flex va-mid">
         {loaded && <button className="land-button" onClick={() => {setHasLanded(true)}}>ENTER</button>}
